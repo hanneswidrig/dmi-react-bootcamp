2f3d0aaa53dee7bbec7f32191a663fc4ff1f4079
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2023-01-24 11:31:27 -0500

    chore: switch to tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,8 @@ export function App() {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div className="App">
-			<div>
+		<div className="flex flex-col justify-center gap-4">
+			<div className="flex justify-center">
 				<a href="https://vitejs.dev" target="_blank">
 					<img src="/vite.svg" className="logo" alt="Vite logo" />
 				</a>
@@ -17,17 +17,14 @@ export function App() {
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+			<div className="flex flex-col gap-4">
+				<button type="button" onClick={() => setCount((count) => count + 1)}>
 					count is {count}
 				</button>
 				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
+					Edit <code>src/App.tsx</code> and save to see changes.
 				</p>
 			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</div>
 	);
 }
