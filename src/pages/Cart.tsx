75f056fe8697336597f1214dee908bab29cc0579
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2023-02-05 16:25:20 -0500

    feat: add & remove

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,16 +1,18 @@
+import { v4 } from "uuid";
 import groupBy from "just-group-by";
 
 import { productList } from "../mock-data/products";
 import { CartItem, useCart } from "../hooks/useCart";
 
 function Item({ groupedItem }: { groupedItem: [productKey: string, items: CartItem[]] }) {
+	const { setCart } = useCart();
 	const [productKey, items] = groupedItem;
 	const totalCost = items.reduce((prev, curr) => prev + curr.price, 0).toFixed(2);
 
 	const matchedItem = productList.find((product) => product.productKey === productKey);
 	if (matchedItem) {
 		return (
-			<div className="flex w-full justify-between rounded-md border-2 border-blue-100 bg-gray-50 text-gray-800">
+			<div className="flex w-full justify-between rounded-md border-2 border-gray-100 bg-gray-50 text-gray-800">
 				<div className="m-4 flex w-full justify-between gap-4">
 					<div className="flex gap-4">
 						<img src={matchedItem.imgSrc} alt="product image" className="h-24 w-24 rounded-full" />
@@ -20,10 +22,36 @@ function Item({ groupedItem }: { groupedItem: [productKey: string, items: CartIt
 						<h1 className="text-lg font-semibold">${totalCost}</h1>
 						<div className="flex gap-2">
 							<span className="px-2 py-1 text-sm font-medium text-gray-800">Qty: {items.length}</span>
-							<button className="cursor-pointer select-none rounded-md bg-green-700 px-2 py-1 text-sm font-medium text-white shadow active:bg-green-800">
+							<button
+								className="cursor-pointer select-none rounded-md bg-green-700 px-2 py-1 text-sm font-medium text-white shadow active:bg-green-800"
+								onClick={() =>
+									setCart((prevItems) => {
+										const addedItem: CartItem = {
+											id: v4(),
+											price: parseFloat(matchedItem.price),
+											productKey: matchedItem.productKey,
+										};
+
+										return [...prevItems, addedItem];
+									})
+								}>
 								Add
 							</button>
-							<button className="cursor-pointer select-none rounded-md bg-red-700 px-2 py-1 text-sm font-medium text-white shadow active:bg-red-800">
+							<button
+								className="cursor-pointer select-none rounded-md bg-red-700 px-2 py-1 text-sm font-medium text-white shadow active:bg-red-800"
+								onClick={() =>
+									setCart((prevItems) => {
+										const matchedItems = prevItems
+											.filter(({ productKey }) => productKey === matchedItem.productKey)
+											.slice(0, -1);
+
+										const nonMatchingItems = prevItems.filter(
+											({ productKey }) => productKey !== matchedItem.productKey
+										);
+
+										return [...matchedItems, ...nonMatchingItems];
+									})
+								}>
 								Remove
 							</button>
 						</div>
